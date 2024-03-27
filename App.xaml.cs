f8b4f4a75000d638066ff7fd584bb3b5ef265f1f
laveha <vdb12122005@gmail.com>
2024-03-27 20:03:14 +0300

    hghghghg

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using Calculator.ViewModels;
+
+namespace Calculator
+{
+    public partial class App : Application
+    {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            Views.CalculatorView view = new Views.CalculatorView();
+            view.DataContext = new ViewModels.CalculatorViewModel();
+            view.Show();
+        }
+    }
+}
